55e4f4cc63b1ee72559ad192f7b8e4ced0c527fe
PawelWosiak-TomTom <Pawel.Wosiak@tomtom.com>
2022-02-16 23:26:30 +0000

    working with products only

--- count-tracker.sh
+++ count-tracker.sh
@@ -1,9 +1,7 @@
 # A simple loop that can be run to check on counts for our two indices as you are indexing.  Ctrl-c to get out.
 while [ true ];
 do
-  echo "Queries:"
-  curl -k -XGET -u admin:admin  "https://localhost:9200/_cat/count/bbuy_queries";
-  echo "Products:"
-  curl -k -XGET -u admin:admin  "https://localhost:9200/_cat/count/bbuy_products";
-  sleep 100;
+  #echo -n "Queries:";  curl -k -XGET -u admin:admin  "https://localhost:9200/_cat/count/bbuy_queries";
+  echo -n "Products:"; curl -k -XGET -u admin:admin  "https://localhost:9200/_cat/count/bbuy_products";
+  sleep 2;
 done
