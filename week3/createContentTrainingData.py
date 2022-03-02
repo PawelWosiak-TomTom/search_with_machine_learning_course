9b0277b97be70ebe9451170e9edd4378a4de9d2a
PawelWosiak-TomTom <Pawel.Wosiak@tomtom.com>
2022-03-02 19:46:15 +0000

    implemented 'min_products'

--- week3/createContentTrainingData.py
+++ week3/createContentTrainingData.py
@@ -3,6 +3,7 @@ import os
 import random
 import xml.etree.ElementTree as ET
 from pathlib import Path
+from collections import defaultdict
 
 def transform_name(product_name):
     return product_name.lower()
@@ -35,6 +36,7 @@ min_products = args.min_products
 sample_rate = args.sample_rate
 
 print("Writing results to %s" % output_file)
+dd = defaultdict(list)
 with open(output_file, 'w') as output:
     for filename in os.listdir(directory):
         if filename.endswith(".xml"):
@@ -49,9 +51,25 @@ with open(output_file, 'w') as output:
                 if (child.find('name') is not None and child.find('name').text is not None and
                     child.find('categoryPath') is not None and len(child.find('categoryPath')) > 0 and
                     child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text is not None):
-                      # Choose last element in categoryPath as the leaf categoryId
-                      cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text
-                      # Replace newline chars with spaces so fastText doesn't complain
-                      name = child.find('name').text.replace('\n', ' ')
-                      output.write("__label__%s %s\n" % (cat, transform_name(name)))
+                        # Choose last element in categoryPath as the leaf categoryId
+                        cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text
+                        # Replace newline chars with spaces so fastText doesn't complain
+                        name = child.find('name').text.replace('\n', ' ')
+                        entry = "__label__%s %s\n" % (cat, transform_name(name))
+                        dd[cat].append(entry)
+
+    filtered_out_entries = 0
+    filtered_in_entries = 0
+    print("Distinct categories count: %s" % len(dd))
+    print("Writing results to %s" % output_file)
+    for (cat, lines) in dd.items():
+        if (len(lines) > min_products):            
+            for line in lines:
+                output.write(line)
+                filtered_in_entries += 1
+        else:
+            filtered_out_entries += len(lines)
+    
+    print("Saved lines %s, filtered out due to 'min_products=%s': %s" % (filtered_in_entries, min_products, filtered_out_entries))
+
 
