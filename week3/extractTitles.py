aef6d7f63b0a9318c729b4229c779d1f4414f2f1
PawelWosiak-TomTom <Pawel.Wosiak@tomtom.com>
2022-03-05 18:05:30 +0000

    script 'extractTitles' modified to take as input 'fasttext' format - works ~100x faster than xml parsing

--- week3/extractTitles.py
+++ week3/extractTitles.py
@@ -4,24 +4,21 @@ import xml.etree.ElementTree as ET
 import argparse
 from pathlib import Path
 
-directory = r'/workspace/search_with_machine_learning_course/data/pruned_products'
 parser = argparse.ArgumentParser(description='Process some integers.')
 general = parser.add_argument_group("general")
-general.add_argument("--input", default=directory,  help="The directory containing the products")
+general.add_argument("--input", default="/workspace/datasets/fasttext/full.fasttext",  help="The file containing the products in fasttext format")
 general.add_argument("--output", default="/workspace/datasets/fasttext/titles.txt", help="the file to output to")
 
 # Consuming all of the product data takes a while. But we still want to be able to obtain a representative sample.
-general.add_argument("--sample_rate", default=0.1, type=float, help="The rate at which to sample input (default is 0.1)")
+general.add_argument("--sample_rate", default=1, type=float, help="The rate at which to sample input (default is 0.1)")
 
 args = parser.parse_args()
+input_file = args.input
 output_file = args.output
 path = Path(output_file)
 output_dir = path.parent
 if os.path.isdir(output_dir) == False:
         os.mkdir(output_dir)
-
-if args.input:
-    directory = args.input
 
 sample_rate = args.sample_rate
 
@@ -29,18 +26,21 @@ def transform_training_data(name):
     # IMPLEMENT
     return name.replace('\n', ' ')
 
+
+def transform_line(line):
+    cat = line.split(' ')[0]
+    product_tab = line.split(' ')[1:]
+    product_str = ' '.join(product_tab)
+    product_transformed = transform_training_data(product_str)
+    return product_transformed
+
 # Directory for product data
 
+print("Processing %s" % input_file)
 print("Writing results to %s" % output_file)
 with open(output_file, 'w') as output:
-    for filename in os.listdir(directory):
-        if filename.endswith(".xml"):
-            f = os.path.join(directory, filename)
-            tree = ET.parse(f)
-            root = tree.getroot()
-            for child in root:
-                if random.random() > sample_rate:
-                    continue
-                if (child.find('name') is not None and child.find('name').text is not None):
-                    name = transform_training_data(child.find('name').text)
-                    output.write(name + "\n")
+    for line in open(input_file):
+        if random.random() > sample_rate:
+            continue
+        product_transformed = transform_line(line)
+        output.write(product_transformed + "\n")        
