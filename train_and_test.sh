5fc35bda47f9cc0f14c93db76a40535beafb32a9
PawelWosiak-TomTom <Pawel.Wosiak@tomtom.com>
2022-03-12 00:38:42 +0000

    updated path, adjusted variants in bulk training

--- train_and_test.sh
+++ train_and_test.sh
@@ -1,103 +1,114 @@
 #!/bin/bash
-set -x
-echo "Test on '-wordNgrams 2': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -wordNgrams 2
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-epoch 1': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -epoch 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
 
-echo "Test on '-wordNgrams 2 -lr 0.1': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -wordNgrams 2 -lr 0.1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-lr 0.1': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -lr 0.1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
 
-echo "Test on '-wordNgrams 2 -lr 0.3': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -wordNgrams 2 -lr 0.3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-lr 0.3': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -lr 0.3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
 
-echo "Test on '-wordNgrams 2 -lr 0.5': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -wordNgrams 2 -lr 0.5
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-lr 0.5': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -lr 0.5
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
 
-echo "Test on '-wordNgrams 2 -lr 1.0': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -wordNgrams 2 -lr 1.0
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-lr 0.9': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -lr 0.9
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
 
-echo "Test on '-wordNgrams 2 -lr 0.1 -epoch 10': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -wordNgrams 2 -lr 0.1 -epoch 10
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-wordNgrams 2': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -wordNgrams 2
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
 
-echo "Test on '-wordNgrams 2 -lr 0.1 -epoch 15': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -wordNgrams 2 -lr 0.1 -epoch 15
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-wordNgrams 2 -lr 0.1': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -wordNgrams 2 -lr 0.1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
 
-echo "Test on '-wordNgrams 2 -lr 0.1 -epoch 20': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -wordNgrams 2 -lr 0.1 -epoch 20
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-wordNgrams 2 -lr 0.3': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -wordNgrams 2 -lr 0.3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
 
-echo "Test on '-wordNgrams 2 -lr 0.1 -epoch 25': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -wordNgrams 2 -lr 0.1 -epoch 25
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-wordNgrams 2 -lr 0.5': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -wordNgrams 2 -lr 0.5
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
 
-echo "Test on '-lr 0.1': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -lr 0.1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-wordNgrams 2 -lr 0.9': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -wordNgrams 2 -lr 0.9
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
 
-echo "Test on '-lr 0.3': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -lr 0.3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-wordNgrams 2 -lr 0.5 -epoch 10': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -wordNgrams 2 -lr 0.5 -epoch 10
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
 
-echo "Test on '-lr 0.5': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -lr 0.5
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-wordNgrams 2 -lr 0.5 -epoch 15': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -wordNgrams 2 -lr 0.5 -epoch 15
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
 
-echo "Test on '-lr 1.0': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -lr 1.0
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-wordNgrams 2 -lr 0.5 -epoch 20': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -wordNgrams 2 -lr 0.5 -epoch 20
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
 
-echo "Test on '-lr 0.1 -epoch 10': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -lr 0.1 -epoch 10
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-wordNgrams 2 -lr 0.5 -epoch 25': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -wordNgrams 2 -lr 0.5 -epoch 25
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
 
-echo "Test on '-lr 0.1 -epoch 15': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -lr 0.1 -epoch 15
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-lr 0.5 -epoch 10': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -lr 0.5 -epoch 10
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
 
-echo "Test on '-lr 0.1 -epoch 20': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -lr 0.1 -epoch 20
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-lr 0.5 -epoch 15': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -lr 0.5 -epoch 15
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
 
-echo "Test on '-lr 0.1 -epoch 25': "
-~/fastText-0.9.2/fasttext supervised -input /workspace/datasets/train.txt -output /workspace/datasets/model -lr 0.1 -epoch 25
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 1
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 3
-~/fastText-0.9.2/fasttext test /workspace/datasets/model.bin /workspace/datasets/test.txt 5
+echo "$(date +"%Y-%m-%d %T") - Test on '-lr 0.5 -epoch 20': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -lr 0.5 -epoch 20
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
+
+echo "$(date +"%Y-%m-%d %T") - Test on '-lr 0.5 -epoch 25': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -lr 0.5 -epoch 25
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
+
+echo "$(date +"%Y-%m-%d %T") - Test on '-wordNgrams 3 -lr 0.5 -epoch 15': "
+~/fastText-0.9.2/fasttext supervised -input /workspace/fasttext/train.txt -output /workspace/fasttext/model -wordNgrams 3 -lr 0.5 -epoch 15
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 1
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 3
+~/fastText-0.9.2/fasttext test /workspace/fasttext/model.bin /workspace/fasttext/test.txt 5
